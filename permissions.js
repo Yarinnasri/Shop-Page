d1eca2eaab35db61c9f25ac08bb1b897f1a29db4
Yarinnasri <yarinnasri@gmail.com>
2023-12-07 19:50:30 +0200

    back to original

--- permissions.js
+++ permissions.js
@@ -6,10 +6,5 @@ const loggedInUser = usersFromSession.find(
   (user) => user[0] === loggedInUserEmail
 )[1];
 
-console.log("loggedInUser " + loggedInUser);
-console.log("loggedInUser[1] " + loggedInUser[1]);
-console.log("loggedInUser.permissions " + loggedInUser.permissions);
-
-userPermissions = new Set(loggedInUser.permissions);
-
+const userPermissions = new Set(loggedInUser.permissions);
 console.log(userPermissions);
